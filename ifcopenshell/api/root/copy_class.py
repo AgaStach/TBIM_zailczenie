67cd310276164e0d494b49f89787157ec993d47e
AgaStach <agstach15@gmail.com>
2023-01-20 19:00:14 +0100

    added library ifcopenshell; added ifc file; load file in wall.py

---
+++ ifcopenshell/api/root/copy_class.py
@@ -0,0 +1,44 @@
+import ifcopenshell
+import ifcopenshell.util.element
+
+
+class Usecase:
+    def __init__(self, file, **settings):
+        self.file = file
+        self.settings = {"product": None}
+        for key, value in settings.items():
+            self.settings[key] = value
+
+    def execute(self):
+        self.schema = ifcopenshell.ifcopenshell_wrapper.schema_by_name(self.file.schema)
+        result = ifcopenshell.util.element.copy(self.file, self.settings["product"])
+        self.copy_indirect_attributes(self.settings["product"], result)
+        # Copying representations is too hard, so for now we just don't do it.
+        self.remove_representations(result)
+        return result
+
+    def copy_indirect_attributes(self, from_element, to_element):
+        for inverse in self.file.get_inverse(from_element):
+            if inverse.is_a("IfcRelDefinesByProperties"):
+                inverse = ifcopenshell.util.element.copy(self.file, inverse)
+                inverse.RelatedObjects = [to_element]
+                pset = ifcopenshell.util.element.copy_deep(self.file, inverse.RelatingPropertyDefinition)
+                inverse.RelatingPropertyDefinition = pset
+            if inverse.is_a("IfcRelAggregates") and inverse.RelatingObject == from_element:
+                continue
+            else:
+                # TODO: Consider whether this general approach is good or not. Maybe it isn't.
+                for i, value in enumerate(inverse):
+                    if value == from_element:
+                        new_inverse = ifcopenshell.util.element.copy(self.file, inverse)
+                        new_inverse[i] = to_element
+                    elif isinstance(value, (tuple, list)) and from_element in value:
+                        new_value = list(value)
+                        new_value.append(to_element)
+                        inverse[i] = new_value
+
+    def remove_representations(self, element):
+        if element.is_a("IfcProduct"):
+            element.Representation = None
+        elif element.is_a("IfcTypeProduct"):
+            element.RepresentationMaps = None
